dfb4f539e571ed73c91b9f30ceaac85189a59459
Alicia Gómez <146170489+PinkyAli@users.noreply.github.com>
2023-12-12 11:16:51 +0100

    Add files via upload

---
+++ Tasca1/S04T01N01AliciaGomez/S04T01N01AliciaGomez/src/main/java/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/HelloWorld")
+	public String saluda(@RequestParam(value = "name", defaultValue = "Unknown") String name) {
+		return "Hola " + name + ". Estas ejecutando un proyecto Maven";
+	}
+	
+	@GetMapping({"/HelloWorld2", "HelloWorld2/{name}"})
+	public String saluda2(@PathVariable(required = false) String name) {
+		if(name == null) {
+			name = "Unknown";
+		}
+		return "Hola " + name + ". Estas ejecutando un proyecto Maven";
+	}
+
+}
