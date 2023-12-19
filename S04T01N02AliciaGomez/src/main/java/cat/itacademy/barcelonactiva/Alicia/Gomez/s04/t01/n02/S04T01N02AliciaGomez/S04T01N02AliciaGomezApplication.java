c2b57ce28215f1f79c2a2d5f2d2e584eef22b3be
Alicia Gómez <146170489+PinkyAli@users.noreply.github.com>
2023-12-19 16:01:01 +0100

    Add files via upload

---
+++ S04T01N02AliciaGomez/src/main/java/cat/itacademy/barcelonactiva/Alicia/Gomez/s04/t01/n02/S04T01N02AliciaGomez/S04T01N02AliciaGomezApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Alicia.Gomez.s04.t01.n02.S04T01N02AliciaGomez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02AliciaGomezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02AliciaGomezApplication.class, args);
+	}
+
+}
---
+++ S04T01N02AliciaGomez/src/test/java/cat/itacademy/barcelonactiva/Alicia/Gomez/s04/t01/n02/S04T01N02AliciaGomez/S04T01N02AliciaGomezApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Alicia.Gomez.s04.t01.n02.S04T01N02AliciaGomez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02AliciaGomezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
